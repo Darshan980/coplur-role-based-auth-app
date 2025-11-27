96f169c72ab8c1daee790760f3082c445b767e3e
Darshan <darshanpoojary980@gmail.com>
2025-11-27 17:44:00 +0530

    commited

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,21 @@
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://coplur-role-based-auth-app-4zug.vercel.app"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
-
 
 const connectDB = require("./config/db");
 const seedAdmin = require("./config/seedAdmin");
@@ -18,7 +24,6 @@ connectDB().then(() => {
 });
 
 const User = require("./UserModel");
-
 
 const register = require("./auth/register");
 app.use("/api/auth/register", register);
@@ -41,9 +46,7 @@ app.use("/api/auth/logout", middleware, logout);
 const welcome = require("./Welcome");
 app.use("/api/welcome", middleware, welcome);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
