0ac6e4f6095010c0d8b35300b6dd361be4c398b9
Darshan <darshanpoojary980@gmail.com>
2025-11-27 08:46:53 +0530

    mangodb datbase Connected

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const connectDB = require("./config/db");
+connectDB();
+
