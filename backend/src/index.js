1e317257f9e968936d296730514aca48c81d4c34
Darshan <darshanpoojary980@gmail.com>
2025-11-27 17:40:26 +0530

    commited

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,15 @@
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const app = express();
-
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://coplur-role-based-auth-app.vercel.app",
-    "https://coplur-role-based-auth-app-*.vercel.app" 
-  ],
-  credentials: true
-}));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
+
 
 const connectDB = require("./config/db");
 const seedAdmin = require("./config/seedAdmin");
@@ -24,13 +17,20 @@ connectDB().then(() => {
   seedAdmin();
 });
 
-const middleware = require("./middleware/auth");
-const admin = require("./middleware/admin");
+const User = require("./UserModel");
+
 
 const register = require("./auth/register");
 app.use("/api/auth/register", register);
+
 const signin = require("./auth/signin");
 app.use("/api/auth/signin", signin);
+
+const middleware = require("./middleware/auth");
+const admin = require("./middleware/admin");
+
+const adminRoutes = require("./admin/adminRoutes");
+app.use("/api/admin", middleware, admin, adminRoutes);
 
 const changePassword = require("./auth/changePassword");
 app.use("/api/auth/change-password", middleware, changePassword);
@@ -38,14 +38,12 @@ app.use("/api/auth/change-password", middleware, changePassword);
 const logout = require("./auth/logout");
 app.use("/api/auth/logout", middleware, logout);
 
-
 const welcome = require("./Welcome");
 app.use("/api/welcome", middleware, welcome);
 
-const adminRoutes = require("./admin/adminRoutes");
-app.use("/api/admin", middleware, admin, adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
