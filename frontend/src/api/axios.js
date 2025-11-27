daff0fdaaafe89a8c3f63960eb2a9a2d606737ec
Darshan <darshanpoojary980@gmail.com>
2025-11-27 17:23:36 +0530

    commited

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: window.location.hostname === "localhost" 
-    ? "http://localhost:5000/api" 
-    : "https://coplur-role-based-auth-app.onrender.com/api",
+    ? "http://localhost:5000/api/auth" 
+    : "https://coplur-role-based-auth-app.onrender.com/api/auth",
 });
 
-// Automatically attach token
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
