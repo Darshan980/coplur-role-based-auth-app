dd8b1b318810d9d77bc99bb5a53c00aa342bb926
Darshan <darshanpoojary980@gmail.com>
2025-11-27 17:15:07 +0530

    commited

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,22 @@
-
 const express = require("express");
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-   "https://coplur-role-based-auth-app-4zug.vercel.app"
-  ],
-  credentials: true
-}));
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://coplur-role-based-auth-app.vercel.app",
+    "https://coplur-role-based-auth-app-*.vercel.app" 
+  ],
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
-
 
 const connectDB = require("./config/db");
 const seedAdmin = require("./config/seedAdmin");
@@ -25,20 +24,13 @@ connectDB().then(() => {
   seedAdmin();
 });
 
-const User = require("./UserModel");
-
-
-const register = require("./auth/register");
-app.use("/api/auth/register", register);
-
-const signin = require("./auth/signin");
-app.use("/api/auth/signin", signin);
-
 const middleware = require("./middleware/auth");
 const admin = require("./middleware/admin");
 
-const adminRoutes = require("./admin/adminRoutes");
-app.use("/api/admin", middleware, admin, adminRoutes);
+const register = require("./auth/register");
+app.use("/api/auth/register", register);
+const signin = require("./auth/signin");
+app.use("/api/auth/signin", signin);
 
 const changePassword = require("./auth/changePassword");
 app.use("/api/auth/change-password", middleware, changePassword);
@@ -46,12 +38,14 @@ app.use("/api/auth/change-password", middleware, changePassword);
 const logout = require("./auth/logout");
 app.use("/api/auth/logout", middleware, logout);
 
+
 const welcome = require("./Welcome");
 app.use("/api/welcome", middleware, welcome);
 
+const adminRoutes = require("./admin/adminRoutes");
+app.use("/api/admin", middleware, admin, adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
